df1e7c03443d17de55c065e9b33bb8ed8762dd86
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-02-15 13:17:55 +0530

    Update connection.js

--- middleware/connection.js
+++ middleware/connection.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+// const connection = mysql.createPool({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"paint_corner"
+// });
+
 const connection = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"paint_corner"
+    host:process.env.HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.DATABASE
 });
 
 module.exports = connection;
