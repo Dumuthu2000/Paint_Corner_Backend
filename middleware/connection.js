d38f6bfb1ed9b3841f5001be6a14b205eaf0f695
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-02-15 13:23:09 +0530

    Update connection.js

--- middleware/connection.js
+++ middleware/connection.js
@@ -9,10 +9,10 @@ require('dotenv').config();
 // });
 
 const connection = mysql.createPool({
-    host:process.env.HOST,
+    host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
     password:process.env.MYSQL_PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.MYSQL_DATABASE
 });
 
 module.exports = connection;
