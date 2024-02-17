ba6456709e912cdd004a4ff4a507f1bd5ac6f702
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-02-17 22:59:29 +0530

    Update connection.js

--- middleware/connection.js
+++ middleware/connection.js
@@ -7,12 +7,17 @@ require('dotenv').config();
 //     password:"",
 //     database:"paint_corner"
 // });
-
+console.log('1111')
+try {
 const connection = mysql.createPool({
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
     password:process.env.MYSQL_PASSWORD,
     database:process.env.MYSQL_DATABASE
 });
+console.log(connection)s
+} catch (e) {
+    console.log(e,'22222222')
+}
 
 module.exports = connection;
