47d8df8ce1b7478d9d3844a663cfe126d32dfc52
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-02-18 00:45:19 +0530

    Update AuthToken.js

--- middleware/AuthToken.js
+++ middleware/AuthToken.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+// const jwt = require('jsonwebtoken');
+// require('dotenv').config();
 
-module.exports = async (req, res, next) => {
-    const authorizationHeader = req.header('Authorization');
+// module.exports = async (req, res, next) => {
+//     const authorizationHeader = req.header('Authorization');
 
-    if (!authorizationHeader) {
-        return res.status(400).json({ Message: "Token doesn't exist" });
-    }
+//     if (!authorizationHeader) {
+//         return res.status(400).json({ Message: "Token doesn't exist" });
+//     }
 
-    const token = authorizationHeader.split(' ')[1];
+//     const token = authorizationHeader.split(' ')[1];
 
-    if (!token) {
-        return res.status(400).json({ Message: "Token is missing" });
-    }
+//     if (!token) {
+//         return res.status(400).json({ Message: "Token is missing" });
+//     }
 
-    try {
-        const payLoad = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = payLoad;
-        next();
-    } catch (error) {
-        res.status(400).json({ Message: "Token is invalid" });
-    }
-};
+//     try {
+//         const payLoad = jwt.verify(token, process.env.SECRET_KEY);
+//         req.user = payLoad;
+//         next();
+//     } catch (error) {
+//         res.status(400).json({ Message: "Token is invalid" });
+//     }
+// };
