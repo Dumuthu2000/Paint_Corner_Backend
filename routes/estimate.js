dd63b5a7ebe50d62c6e353711bc5d56783f1eff5
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-02-18 23:17:40 +0530

    Update estimate.js

--- routes/estimate.js
+++ routes/estimate.js
@@ -2,19 +2,17 @@ const express = require('express');
 const router = express.Router();
 const estimateController = require('../controller/estimateController');
 
-//Search job from job table
-router.get('/searchEstimateJob/:jobID', estimateController.searchJob);
-
-//Search job from estimate table
-router.get('/searchJob/:jobID', estimateController.searchEstimateJob);
-
 //Fetch Items
 router.get('/items', estimateController.getItems);
+
 
 //Creaet new Estimate
 router.post('/createEstimate', estimateController.createEstimate);
 
-//Check available Estimates job no
-router.get('/availableEstimates', estimateController.checkEstimates);
+//Fetch Items
+router.get('/getEstimateJobDetails/:jobID', estimateController.searchJobForEstimatePreview);
 
+
+//Get Insurance Total
+router.get('/searchJobCardForCreateEstimate/:jobID', estimateController.searchJobCardForCreateEstimate);
 module.exports = router;
